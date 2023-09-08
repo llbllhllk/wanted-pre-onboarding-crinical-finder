98c7219deb940d4c987950ec31fd3b81e04e64c3
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-09-08 19:39:52 +0900

    Feat: useClickOutside hook 구현

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import React from 'react';
+
+function useClickOutside(
+  ref: React.MutableRefObject<HTMLInputElement | null>,
+  callback: () => void,
+) {
+  useEffect(() => {
+    function handleClickOutside(e: MouseEvent) {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        callback();
+      }
+    }
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [ref, callback]);
+}
+
+export default useClickOutside;
